86542157a2bc21dc78872df745d06ba4d3ed2fba
m23sz <m23sz.dev@gmail.com>
2024-08-08 02:23:18 +0200

    Added main router

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const { router } = require('./server/routes/main');
+
 const app = express();
 const PORT= 5000 || process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
+
+app.use('/', router)
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
